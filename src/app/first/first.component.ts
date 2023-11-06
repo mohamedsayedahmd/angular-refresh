425883f07420cfc2ed113c971ff6c3dfa7cbefd5
mohamedsayedahmd <mohamedsayedahmd@gmail.com>
2023-11-06 16:05:58 +0200

    delete hardcore and some style

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first.component.css'],
 })
 export class FirstComponent implements OnInit {
+  hero = {
+    name: '',
+    power: 0,
+    image: '',
+  };
+
+  heros: any[] = [];
+
   constructor() {}
   ngOnInit() {}
+
+  ajout() {
+    this.heros.push(this.hero);
+    this.hero = {
+      name: '',
+      power: 0,
+      image: '',
+    };
+    console.log(this.heros);
+  }
 }
